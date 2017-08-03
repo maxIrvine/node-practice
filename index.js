45332412d86af01f1c0f1c019a38d53c9ad4ce4b
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-08-03 10:05:37 -0400

    initial version of filereader

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("filename: ", (filename) => {
+    rl.close();
+    fs.readFile(filename, (err,buffer) => {
+        if (err) {
+            console.log(err.message);
+            return;
+        }
+        let content = buffer.toString();
+        let upcased = content.toUpperCase();
+        console.log(upcased);
+    });
+});
